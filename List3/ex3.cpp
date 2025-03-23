e1d732c321ae195f06356305d91f33e4df4def8b
Paulo CG <paulo.h.r.correa@gmail.com>
2025-03-22 20:01:54 -0400

    primeira lista feita

--- List3/ex3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-enum civil_status { SINGLE, MARRIED, DIVORCED, WIDOWER};
-
-int main(int argc, char const *argv[])
-{
-    return 0;
-}
--- List4/ex4.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int *v;
-    v = (int *) malloc(10 * sizeof(int)) ;
-    return 0;
-}
